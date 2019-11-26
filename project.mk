99f6f3d4431c10316ff7a14bb852c97de54d819e
Dustin Richmond <dustinar@uw.edu>
2019-11-26 08:47:53 -0800

    fixup! Update for v3.5.0

--- project.mk
+++ project.mk
@@ -39,5 +39,5 @@ BASEJUMP_STL_COMMIT_ID := $(shell cd $(BASEJUMP_STL_DIR); git rev-parse --short 
 
 FPGA_IMAGE_VERSION     := 3.5.0
 F12XLARGE_TEMPLATE_ID  := lt-01bc73811e48f0b26
-AFI_ID                 := afi-0006478ab89782568
-AGFI_ID                := agfi-0f962acc50ea01933
+AFI_ID                 := 
+AGFI_ID                := 
