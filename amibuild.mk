607ea2ef61f2baaa4fb99d2e092ba4e1e71da00f
Dustin Richmond <dustinar@uw.edu>
2019-12-03 11:21:34 -0800

    v3.5.0 (#43)
    * BSG Manycore:
      * Changed Toolchain naming from unknown to dramfs
      * Added new CUDA-Lite kernels
      * Changed implementation of configurable memory system in Simulation RTL
      * Added non-blocking Cache
      * Added Stats Parser
    
    BaseJump:
      * Unrelated changes
    
    BSG F1:
      * More CUDA Kernels
      * Changed implementation of configurable memory system in Simulation RTL
      * Implemented clock-switching IO-speedup
      * Updated Git on AWS Instance
    
    Bladerunner: 
    * Removed aws-fpga.patch
    * Removed patch command in amibuild.mk

--- amibuild.mk
+++ amibuild.mk
@@ -62,7 +62,6 @@ AWS_FPGA_REPO_DIR := $(BLADERUNNER_ROOT)/aws-fpga
 setup-aws-fpga: $(AWS_FPGA_REPO_DIR).setup.log
 $(AWS_FPGA_REPO_DIR).setup.log:
 	git submodule update --init aws-fpga
-	cd $(AWS_FPGA_REPO_DIR); patch -p0 < $(BLADERUNNER_ROOT)/aws-fpga.patch
 	. $(AWS_FPGA_REPO_DIR)/hdk_setup.sh > $@
 
 RISCV_TOOLS_DIR=$(BSG_MANYCORE_DIR)/software/riscv-tools/
