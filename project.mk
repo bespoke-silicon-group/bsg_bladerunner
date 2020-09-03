7e2b87cda4a51193c926a0690e6c5fb1fe09d5e5
Dustin Richmond <dustinar@uw.edu>
2020-09-03 11:30:43 -0700

    v4.3.0 (#57)
    * Reduced Newlib static variables
    * Introduced new attributes for no-alias and remote
    * Fixed bug in profiler
    * Updated version number
    * Updated project.mk with AGFI/AFI

--- project.mk
+++ project.mk
@@ -37,7 +37,7 @@ BSG_MANYCORE_COMMIT_ID := $(shell cd $(BSG_MANYCORE_DIR); git rev-parse --short 
 BASEJUMP_STL_DIR       := $(BLADERUNNER_ROOT)/basejump_stl
 BASEJUMP_STL_COMMIT_ID := $(shell cd $(BASEJUMP_STL_DIR); git rev-parse --short HEAD)
 
-FPGA_IMAGE_VERSION     := 4.2.1
+FPGA_IMAGE_VERSION     := 4.3.0
 F12XLARGE_TEMPLATE_ID  := lt-01bc73811e48f0b26
-AFI_ID                 := afi-01b19289780f5d700
-AGFI_ID                := agfi-01ae314b764d0573d
+AFI_ID                 := afi-0ca6bb25754322f0f
+AGFI_ID                := agfi-07014e89a833565e7
